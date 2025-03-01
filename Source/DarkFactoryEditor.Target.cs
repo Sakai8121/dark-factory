02c50db189a5d09242ad0f3aaee49c387d9791eb
PC-PM750BAL\sucre <sucre31028@gmail.com>
2025-03-01 16:57:04 +0900

    config git control

--- Source/DarkFactoryEditor.Target.cs
+++ Source/DarkFactoryEditor.Target.cs
@@ -11,5 +11,6 @@ public class DarkFactoryEditorTarget : TargetRules
 		DefaultBuildSettings = BuildSettingsVersion.V5;
 		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_5;
 		ExtraModuleNames.Add("DarkFactory");
+		ExtraModuleNames.Add("YourModuleName");
 	}
 }
